161b77ceac5920e753a858669a88eee2bb4b92cc
CS Lab3  Computer Science Lab3 <cslab3@shu.edu.pk>
2024-12-04 14:43:43 +0500

    Done

---
+++ ATM.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int num;
+	cout <<"\nMenu. \nBalance. \nDepositMoney. \nWithdrawMoney \nExit."
+	     do{
+	     	switch ()
+	     	case 1: Display Balance;
+		 break;
+		 
+		 case 2: Display Deposit Amount;
+		 break;
+		 
+		 case 3: Display Withdraw Money;
+		 break;
+		 
+		 case 4:break;
+		 
+		 Default: "Invalid choice or withdrawal not possible\n." 
+		 }
+		
+		while ( != 0 )
+		
+		if(userBalance < 0 ) {
+			cout << "Money cannot withdraw";
+		} else if(deposit and withdrawal amounts are even )
+		cout << "Positive numbers";
+	   } else if(current balance < withdrawal amount )
+	   cout << "Display error";
+	   else
+	   cout << "Invalid option";
+	   }
+       
+	   return 0;		 
+	    
+	     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
